5c1de7de6d42a1ca956283a7afa55548e380f8c0
Marhead <clgns0415@gmail.com>
2020-02-11 15:31:04 +0900

    Update PICNIC.cpp

--- SongChiHoon/PICNIC.cpp
+++ SongChiHoon/PICNIC.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-bool friendship [10][10] = {};
+bool friendship[10][10] = {};
 int countingarray;
 
 int main()
 {
-    int casenum, studentnum, friendnum = 0;
+	int casenum, studentnum, friendnum, temp = 0;
 
-    cout << "케이스 수 입력 : ";    cin >> casenum;
+	cout << "케이스 수 입력 : ";    cin >> casenum;
 
-    while(casenum != 0)
-    {
-        cout << "학생 수와 친구 수 입력 : ";    cin >> studentnum;  cin >> friendnum;
-        cout << endl << "친구 쌍 입력하기" << endl;
-        for (int i = 0; i < friendnum)
+	while (casenum != 0)
+	{
+		cout << "학생 수와 친구 수 입력 : ";    cin >> studentnum;  cin >> friendnum;
+		cout << endl << "친구 쌍 입력하기" << endl;
+
+		vector<bool> friendinfo;
+
+		for (int i = 0; i < friendnum * 2; i++)
+			cin >> temp;
+			friendinfo.push_back(temp);
 
 
-        casenum--;
-    }
+		casenum--;
+	}
+
+	return 0;
 
 }
