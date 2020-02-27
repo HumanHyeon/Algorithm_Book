6880e1f64d0b25f391c229e9ed47e42361939ec0
Marhead <clgns0415@gmail.com>
2020-02-28 04:45:38 +0900

    Create DP200223.cpp

---
+++ SongChiHoon/Daily_Programming/DP200223.cpp
@@ -0,0 +1,73 @@
+// 길이가 같은 두 문자열(string) A 와 B가 주어지면, A 가 B 로 1:1 암호화 가능한지 찾으시오.
+
+// Given two strings of equal length, check if two strings can be encrypted 1 to 1.
+
+// 예제)
+
+// Input: “EGG”, “FOO”
+
+// Output: True // E->F, G->O
+
+
+
+// Input: “ABBCD”, “APPLE”
+
+// Output: True // A->A, B->P, C->L, D->E
+
+
+
+// Input: “AAB”, “FOO”
+
+// Output: False
+
+#include <iostream>
+using namespace std;
+
+bool matching(char A[], char B[])
+{
+    int temp1, temp2, ret[26], count[26];
+
+    for(int i = 0; i < strlen(A); i++)
+    {
+        temp1 = A[i] - 65;      // ret 배열에서 알파뱃의 위치 정보
+        temp2 = A[i] - B[i];    // 위치적 의미 함유
+
+        if(ret[temp1] == 0)
+        {
+            ret[temp1] = temp2;
+        }
+        else if(ret[temp1] != temp2)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    char * Original;    char * Comparison;
+
+    cout << "Input : ";
+    cin >> Original;    cin >> Comparison;
+
+    matching(Original, Comparison);
+
+    int Length = strlen(Original);
+
+    if(Length == strlen(Comparison))
+    {
+        if(matching(Original, Comparison) == true)
+        {
+            cout << "True";
+        }
+    }
+
+    else
+    {
+        cout << "False";
+    }
+
+    return 0;
+}
