981097816d0ed6afd044ff6a0d8a32bcedbca1e1
y00913 <42912205+y00913@users.noreply.github.com>
2020-04-01 23:02:30 +0900

    Create 매프200329.cpp

---
+++ ayt/매프200329.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+typedef int element;
+typedef struct ListNode
+{
+    element data;
+    struct ListNode* link;
+} ListNode;
+
+void Sorting(ListNode* h1, ListNode* h2)
+{
+    int tmp[10];
+
+    ListNode* p1 = h1; //연결 리스트 연결
+    p1->link = h2;     
+
+    ListNode* p2 = h1; //연결 리스트 데이터를 배열에 추가
+    while (p2 != NULL)
+    {
+        int i = 0;
+        tmp[i] = p2->data;
+        p2 = p2->link;
+        i++;
+    }
+
+    sort(tmp, tmp + sizeof(tmp) / sizeof(*tmp)); //배열 정렬
+
+    ListNode* p3 = h1; //정렬된 데이터를 연결 리스트 데이터와 교환
+    while (p3 != NULL)
+    {
+        int i = 0;
+        p3->data = tmp[i];
+        p3 = p3->link;
+        i++;
+    }
+}
+
+void display(ListNode* h1)
+{
+    ListNode* p = h1;
+    while (p != NULL) {
+        cout << p->data << "->";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ListNode* p1 = (ListNode*)malloc(sizeof(ListNode));
+    p1->data = 1;
+    ListNode* p2 = (ListNode*)malloc(sizeof(ListNode));
+    p1->link = p2;
+    p2->data = 2;
+    ListNode* p3 = (ListNode*)malloc(sizeof(ListNode));
+    p2->link = p3;
+    p3->data = 3;
+    p3->link = NULL;
+
+    ListNode* q1 = (ListNode*)malloc(sizeof(ListNode));
+    q1->data = 1;
+    ListNode* q2 = (ListNode*)malloc(sizeof(ListNode));
+    q1->link = q2;
+    q2->data = 2;
+    ListNode* q3 = (ListNode*)malloc(sizeof(ListNode));
+    q2->link = q3;
+    q3->data = 3;
+    q3->link = NULL;
+
+    Sorting(p1, p2);
+
+    display(p1);
+
+    return 0;
+}
