069be36089ff9b4257f2d8319022a9b864f6c389
Marhead <clgns0415@gmail.com>
2020-06-15 08:34:57 +0900

    ddd

---
+++ SongChiHoon/Programmers/Pipemoving.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int r = 5;  int c = 5;
+
+struct Newhouse
+{
+    int house[r][c] = {};
+};
---
+++ SongChiHoon/Programmers/WatermelonSugarHigh.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string main()
+{
+    int n;
+    string answer = "";
+    cin >> n;
+
+    int size = n / 2;
+    
+    for(int i = 0 ; i < size; i++)
+    {
+        answer += "수박";
+    }    
+    if(n%2 != 0)
+        answer += "수";
+
+    return answer;
+}
