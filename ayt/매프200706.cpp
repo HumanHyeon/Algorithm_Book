23af536b9eb9e1652464d5f4b1ec2eda1a069039
y00913 <42912205+y00913@users.noreply.github.com>
2020-07-11 16:55:56 +0900

    Create 매프200706.cpp

---
+++ ayt/매프200706.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int count1(int inp) {
+	int result = 0;
+
+	while (1) {
+		if (inp == 1) {
+			++result;
+			break;
+		}
+
+		if (inp % 2 == 1)
+			++result;
+		
+		inp /= 2;
+	}
+
+	return result;
+}
+
+int main() {
+	int input;
+
+	cout << "input : ";
+	cin >> input;
+	cout << "output : " << count1(input) << endl;
+
+	return 0;
+}
