ab62b339479e8a6d326f25d1537604ae9c5e698d
y00913 <42912205+y00913@users.noreply.github.com>
2020-07-20 20:08:49 +0900

    Update 매프200622.cpp

--- ayt/매프200622.cpp
+++ ayt/매프200622.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void smallInt(int *inp, int n) {
-	int tmp, sum = 0;
+int smallInt(int inp[], int n) {
+	int ck[100] = { 0, };
 
-	if (*inp != 1)
-		cout << "output : 1";
-	else {
-		for (int i = 1; i <= n; ++i) {
-			if (*inp == i)
-				++inp;
-			else if (*inp != i) {
-				tmp = i;
-				break;
-			}
+	if (inp[0] != 1)
+		return 1;
+	if (inp[1] != 2)
+		return 2;
+
+	for (int i = 0; i < n; ++i) {
+		ck[inp[i]] = 1;
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = i + 1; j < n; ++j) {
+			ck[inp[i] + inp[j]] = 1;
 		}
-
-		for (int i = 1; i < tmp; i++) {
-			sum += i;
+	}
+	for (int i = 1; i < 100; ++i) {
+		if (ck[i] == 0) {
+			return i;
+			break;
 		}
-
-		cout << "output : " << ++sum;
 	}
 }
 
 int main() {
-	int input[] = { 1,2,3,8 };
+	int input[] = { 1,2,3,6,10 };
 	int size = sizeof(input) / sizeof(int);
 
-	smallInt(input, size);
+	cout << "output : " << smallInt(input, size);
 
 	return 0;
 }
