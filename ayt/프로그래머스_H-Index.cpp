8044152d2ef7f083ec8b10e8cb8b3b91d65baa46
y00913 <42912205+y00913@users.noreply.github.com>
2020-07-24 16:08:51 +0900

    Update 프로그래머스_H-Index.cpp

--- ayt/프로그래머스_H-Index.cpp
+++ ayt/프로그래머스_H-Index.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
-
+    
     sort(citations.rbegin(), citations.rend());
     while(1){
-        if(citations[answer]<=answer) break;
+        if(citations[answer]<=answer||answer>citations.size()) break;
         ++answer;
     }
-
+    
     return answer;
 }
