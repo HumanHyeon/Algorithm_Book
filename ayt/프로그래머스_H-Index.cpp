8e02020dec85780c5d82cad083af84145d9310ea
y00913 <42912205+y00913@users.noreply.github.com>
2020-07-24 13:57:42 +0900

    Create 프로그래머스_H-Index.cpp

---
+++ ayt/프로그래머스_H-Index.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+
+    sort(citations.rbegin(), citations.rend());
+    while(1){
+        if(citations[answer]<=answer) break;
+        ++answer;
+    }
+
+    return answer;
+}
