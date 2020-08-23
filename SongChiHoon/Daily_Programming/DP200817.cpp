0563e4307824aa33c390cf80273cdbc532295c5b
Marhead <clgns0415@gmail.com>
2020-08-23 20:37:09 +0900

    Update DP200817.cpp

--- SongChiHoon/Daily_Programming/DP200817.cpp
+++ SongChiHoon/Daily_Programming/DP200817.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 bool isfourpower(int input) {
     int editinput = input / 4;
-    if (editinput % 4 != 0 || editinput < 4) {
+    if (editinput % 4 != 0) {
         return false;
     }
-    else if(editinput == 1) {
+    else {
+        isfourpower(editinput);
         return true;
     }
-    return isfourpower(editinput);
 }
 
 int main() {
     int input;
-    while (input <= 0) {
-        cout << "input : ";
-        cin >> input;
+    do {
+        cout << "input : "; cin >> input;
         if (input <= 0) {
             cout << "Input number must bigger than 0" << endl;
+            cout << "input : "; cin >> input;
         }
-    }
+    } while (input == NULL);
     if (isfourpower(input)) {
         cout << "Output : true";
     }
