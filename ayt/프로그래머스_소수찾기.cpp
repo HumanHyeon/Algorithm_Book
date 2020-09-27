aacee4206ca984982d0d4bcad875c26ba5d4d8b6
y00913 <42912205+y00913@users.noreply.github.com>
2020-09-28 01:01:22 +0900

    Create 프로그래머스_소수찾기.cpp

---
+++ ayt/프로그래머스_소수찾기.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int cache[7];
+int answer;
+vector<int> v;
+set<int> s;
+int answer2 = 0;
+
+void findPrime(int n) {
+    bool tf = true;
+
+    if (n==0 || n == 1) return;
+    
+    for (int i = 2; i < n; ++i) {
+        if (n % i == 0) {
+            tf = false;
+            break;
+        }
+    }
+    
+    if (tf)
+        ++answer2;
+}
+
+
+void dfs(int cnt, int n, string numbers) {
+    if (cnt == n) {
+        int ten = 1, tmp = 0;
+        
+        for (int i = v.size() - 1; i >= 0; --i) {
+            tmp += v[i] * ten;
+            ten *= 10;
+        }
+        s.insert(tmp);
+    }
+    for (int i = 0; i < numbers.size(); ++i) {
+        if (cache[i]) continue;
+        cache[i] = true;
+        v.push_back(numbers[i] - '0');
+        dfs(cnt + 1, n, numbers);
+        v.pop_back();
+        cache[i] = false;
+    }
+}
+
+int solution(string numbers) {
+    for (int i = 1; i <= numbers.size(); ++i)
+        dfs(0, i, numbers);
+    for (auto elem : s)
+        findPrime(elem);
+    
+    answer = answer2;
+
+    return answer;
+}
