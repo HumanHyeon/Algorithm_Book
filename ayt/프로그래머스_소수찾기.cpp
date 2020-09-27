343bbb3b445b208b3b5bc7b221fb7b9f198a352f
y00913 <42912205+y00913@users.noreply.github.com>
2020-09-28 01:08:24 +0900

    Update 프로그래머스_소수찾기.cpp

--- ayt/프로그래머스_소수찾기.cpp
+++ ayt/프로그래머스_소수찾기.cpp
@@ -14,7 +14,10 @@ void findPrime(int n) {
     bool tf = true;
 
     if (n==0 || n == 1) return;
-    
+    if (n==2) {
+        ++answer2;
+        return;
+    }
     for (int i = 2; i < n; ++i) {
         if (n % i == 0) {
             tf = false;
