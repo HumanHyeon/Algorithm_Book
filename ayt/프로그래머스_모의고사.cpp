3ac2e3789ca006fbe3fe7bcf3285efc46cfeb156
y00913 <42912205+y00913@users.noreply.github.com>
2020-10-05 00:13:34 +0900

    Update 프로그래머스_모의고사.cpp

--- ayt/프로그래머스_모의고사.cpp
+++ ayt/프로그래머스_모의고사.cpp
@@ -14,12 +14,14 @@ vector<int> solution(vector<int> answers) {
     vector<int> result = {0,0,0};
     int cnt=0;
     
+    //1번 수포자
     for(int i=0;i<answers.size();++i){
         if(cnt==spz[0].size()) cnt=0;  
         if(answers[i]==spz[0][cnt]) ++result[0];
         
         ++cnt;
     }
+    //2번 수포자
     cnt=0;
     for(int i=0;i<answers.size();++i){
         if(cnt==spz[1].size()) cnt=0;      
@@ -27,6 +29,7 @@ vector<int> solution(vector<int> answers) {
         
         ++cnt;
     }
+    //3번 수포자
     cnt=0;
     for(int i=0;i<answers.size();++i){
         if(cnt==spz[2].size()) cnt=0;
@@ -35,6 +38,7 @@ vector<int> solution(vector<int> answers) {
         ++cnt;
     }
     
+    //결과
     int max = *max_element(result.begin(), result.end());
     for(int i=0;i<3;++i){
         if(max==result[i]) answer.push_back(i+1);
