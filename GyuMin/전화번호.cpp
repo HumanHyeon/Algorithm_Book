67e66d4b362dbb5a99be8809687e65480dfe2d2b
kgm-930 <72342542+kgm-930@users.noreply.github.com>
2020-12-03 19:08:12 +0900

    Create 전화번호.cpp

---
+++ GyuMin/전화번호.cpp
@@ -0,0 +1,26 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42577
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book)
+{
+    bool answer = true;
+
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 0; i < phone_book.size(); i++)
+    {
+
+        for (int j = i + 1; j < phone_book.size(); j++)
+            if (phone_book[i] == phone_book[j].substr(0, phone_book[i].size()))
+            {
+                answer = false;
+                return answer;
+            }
+    }
+
+    return answer;
+}
