7fdca894b2d1f638048b7771d77885813eaf1f90
kgm-930 <72342542+kgm-930@users.noreply.github.com>
2020-12-13 17:18:11 +0900

    Create 124.cpp

---
+++ GyuMin/124.cpp
@@ -0,0 +1,43 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12899
+#include <string>
+
+using namespace std;
+
+string solution(int n)
+{
+	string answer = "";
+
+	for (;;)
+	{
+		int a;
+
+		switch (n % 3)
+		{
+		case 0:
+			answer = "4" + answer;
+			a = 3;
+			break;
+
+		case 1:
+			answer = "1" + answer;
+			a = 1;
+			break;
+
+		case 2:
+			answer = "2" + answer;
+			a = 2;
+			break;
+		}
+
+		if (n <= 3)
+			break;
+
+		else if (!(n % 3))
+			n = (n - 1) / 3;
+
+		else
+			n = n / 3;
+	}
+
+    return answer;
+}
