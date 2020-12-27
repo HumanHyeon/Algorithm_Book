15b07681d5115e1c3acdc8d4d5561a8e1a53d04a
Marhead <clgns0415@gmail.com>
2020-12-27 21:01:42 +0900

    update trianglesnail.cpp

--- SongChiHoon/Programmers/trianglesnail.cpp
+++ SongChiHoon/Programmers/trianglesnail.cpp
@@ -3,93 +3,71 @@
 
 using namespace std;
 
-// vector<int> solution(int n) {
-//     vector<int> answer;
-
-//     //임시 배열 공간 할당
-//     int size = 0;
-//     for(int i = 1; i <= n; i++) { size += i; }
-//     int *temp = new int[size];
-    
-//     //왼쪽 내려가는 대각선
-//     int laststartpoint;
-//     int num = 1;
-//     for (int i = 0; num != n; num++) {
-//         temp[i] = num;
-//         i += num;
-//         laststartpoint = i;
-//     }
-
-//     //삼각형의 밑변
-//     int input = n;
-//     for (int i = laststartpoint; i <= size; i++) { temp[i] = input++; }
-
-//     //삼각형 우측 변
-
-//     answer.assign(temp, temp + size);
-//     return answer;
-// }
-
 vector<int> solution(int n) {
     vector<int> answer;
 
-    // 방향 지시기
+    // Navigation
     bool down = true;
     bool up = false;
 
+    //  Make double array
     int **temp = new int*[n];
     for(int i = 0; i < n; i++) {
         temp[i] = new int[n];
     }
 
-    int endpoint = n;
-    int endvalue;
+    // Setting preset
+    int endvalue = n * (n+1) / 2;
     int startvalue = 1;
     int row, column = 0;
 
-    for(int k = n; k > 0; k--) { endvalue += k; }
-
+    // input algorithm
     for(int input = 1; input != endvalue; input++) {
+
+        // going down
         if(down == true && up == false) {
             temp[row][column] = input;
             row++;
-            if(temp[row][column] != NULL || row == n) {
+            if(row == n || temp[row][column] != 0) {
                 down = false;
                 row--;
                 column++;
             }
         }
+
+        // going side
         else if(down == false && up == false) {
             temp[row][column] = input;
             column++;
-            if(temp[row][column] != NULL || column == n) {
+            if(column == n || temp[row][column] != 0) {
                 up = true;
                 column--;
                 column--;
                 row--;
             }
-
         }
+
+        // going up
         else if(down == false && up == true) {
             temp[row][column] = input;
             row--;
             column--;
-            if(temp[row][column] != NULL || row == 0 || column == 0) {
+            if(row == 0 || column == 0 || temp[row][column] != 0) {
                 up = false;
                 down = true;
                 row++;
                 row++;
+                column++;
             }
         }
     }
 
-    // for(int start = 0; start < n; start++) {
-    //     temp[start][0] = start + 1;
-    // }
-
-    // for(int start = 0; start < n; start++) {
-    //     temp[n-1][start] = n+start;
-    // }
+    // Pushing answer
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            answer.push_back(temp[i][j]);
+        }
+    }
 
     return answer;
 }
