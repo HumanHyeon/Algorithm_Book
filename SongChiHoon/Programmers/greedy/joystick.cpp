35b1d10c7f415f7bfed308afa3e17324e0d2d37a
Marhead <clgns0415@gmail.com>
2020-12-29 11:30:33 +0900

    create PS

---
+++ SongChiHoon/Programmers/greedy/joystick.cpp
@@ -0,0 +1,11 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42860?language=cpp
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string name) {
+    int answer = 0;
+    return answer;
+}
---
+++ SongChiHoon/Programmers/groundpicking.cpp
@@ -0,0 +1,15 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12913?language=cpp
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
