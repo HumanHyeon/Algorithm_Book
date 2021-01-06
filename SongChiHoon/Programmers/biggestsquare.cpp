fe7026e0cbf30a31d6c18eea41fc5c8155831b7e
marhead <clgns0415@gmail.com>
2021-01-06 09:06:59 +0900

    update scoville

--- SongChiHoon/Programmers/biggestsquare.cpp
+++ SongChiHoon/Programmers/biggestsquare.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int squaresearch(vector<vector<int>> input, int startpoint) {
+    int size;
+
     
     return size;
 }
@@ -11,6 +13,10 @@ int solution(vector<vector<int>> board) {
     int answer = 1234;
     int vertical_length = board.size();
     int horizontal_length = board[0].size();
+
+    for(int i = 0; i < vertical_length; i++) {
+        board
+    }
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     cout <<  << endl;
---
+++ SongChiHoon/Programmers/scoville.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+
+    int *temp = new int[scoville.size()];
+
+    temp = 
+    return answer;
+}
