c9aa7444c9bb3ecbf835723bf238b7cecc6314c5
msrgo96 <53008468+msrgo96@users.noreply.github.com>
2021-01-16 04:33:08 +0900

    Update correctBrackets.cpp

--- MoonSeong/correctBrackets.cpp
+++ MoonSeong/correctBrackets.cpp
@@ -1,8 +1,5 @@
 #include <string>
 #include <vector>
-
-#include <iostream>
-
 using namespace std;
 
 string reverseBracket(string s) {
@@ -13,37 +10,49 @@ string reverseBracket(string s) {
     return res;
 }
 
-// -2: nullString -1:correct, 0:unbalanced, n:string[0..n] is least balanced
+// return Index where string[0..n] is the least balanced
 int checkBracket(string s) {
     int balance = 0;
     int balancedIndex = 0;
-    bool isCorrect = true;
     bool flag = true;
-    
+
     for (int i = 0; i < s.length(); i++) {
         if (s.at(i) == '(') balance++;
         if (s.at(i) == ')') balance--;
-        if (balance < 0) isCorrect = false;
-        if (flag && balance == 0) {
-            flag == false;
+        if (flag == true && balance == 0) {
+            flag = false;
             balancedIndex = i;
         }
     }
-    
-    if (s.length == 0) return -2;       //nullString
-    if (balance != 0) return 0;         //unbalanced
-    if (isCorrect) return -1;           //correct
-    //none of all: balanced
-    return return balancedIndex;
+
+    return balancedIndex;
+}
+
+bool isCorrectBracket(string s) {
+    int balance = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s.at(i) == '(') balance++;
+        if (s.at(i) == ')') balance--;
+        if (balance < 0) return false;
+    }
+    return true;
 }
 
 string solution(string p) {
-    string answer = "";
     string u, v;
-    
+    string answer;
+    int check;
+
     if (p.length() == 0) return p;
-    //하는중^^;;
-    
-    
+
+    check = checkBracket(p);
+    u = p.substr(0, check + 1);
+    v = p.substr(check + 1, p.length() - check - 1);
+
+    if (isCorrectBracket(u))
+        answer = u + solution(v);
+    else
+        answer = '(' + solution(v) + ')' + reverseBracket(u.substr(1, u.length() - 2));
+
     return answer;
 }
