aa76769795f911313a457630516d22b55ff2882f
chldudtj1111 <71889004+chldudtj1111@users.noreply.github.com>
2021-01-23 10:44:31 +0900

    Update 비밀지도.cpp

--- YoungSeo/비밀지도.cpp
+++ YoungSeo/비밀지도.cpp
@@ -12,9 +12,9 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
         line = bitset<16>(arr1[i] | arr2[i]).to_string();
         for (int j = 16 - n; j < 16; j++) {
             if (s3[j] == '1') s3[j] = '#';
-            else line[j] = ' ';
+            else s3[j] = ' ';
         }
-        answer.push_back(s3.substr(16-n, 16)); line = "";
+        answer.push_back(s3.substr(16-n, 16)); s3 = "";
     }
     return answer;
 }
