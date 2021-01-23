9c9036490a79e33bc8d801a8a0da49849ecc3e6c
y00913 <42912205+y00913@users.noreply.github.com>
2021-01-23 13:05:57 +0900

    Create 프로그래머스_비밀지도.cpp

---
+++ ayt/프로그래머스_비밀지도.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+
+    for (int i = 0; i < n; ++i) {
+        string tmp;
+
+        arr1[i] = arr1[i] | arr2[i];
+        while (arr1[i] > 0) {
+            (arr1[i] % 2) ? tmp = '#' + tmp : tmp = ' ' + tmp;
+            arr1[i] /= 2;
+        }
+        while (tmp.size() != n) tmp = ' ' + tmp;
+        answer.push_back(tmp);
+    }
+
+    return answer;
+}
