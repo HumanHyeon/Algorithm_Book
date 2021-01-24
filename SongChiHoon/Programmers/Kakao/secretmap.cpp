561d19e267d276a6f92350fb4573efad43347c6f
Marhead <clgns0415@gmail.com>
2021-01-24 20:20:08 +0900

    Create secretmap.cpp

---
+++ SongChiHoon/Programmers/Kakao/secretmap.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+
+    for (int i = 0; i < n; i++)
+    {
+        temp = bitset<n>(arr1[i]).to_string
+    }
+    
+    
+
+    return answer;
+}
