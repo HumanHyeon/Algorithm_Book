5ec7f03273e4ab6f1c7a0ebb12b918e66cf2418a
y00913 <42912205+y00913@users.noreply.github.com>
2021-01-24 14:30:26 +0900

    Create 프로그래머스_실패율.cpp

---
+++ ayt/프로그래머스_실패율.cpp
@@ -0,0 +1,31 @@
+#include <utility>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<double, int>& a, pair<double, int>& b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first > b.first;
+}
+
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
+    vector<pair<double, int>> p;
+    int si = stages.size();
+
+    for (int i = 0; i < N; ++i) {
+        double tmp = 0;
+        for (int j = 0; j < stages.size(); ++j)
+            if (stages[j] == i + 1) ++tmp;
+        if (tmp == 0) p.push_back(make_pair(0, i + 1));
+        else p.push_back(make_pair(tmp / si, i + 1));
+        si -= tmp;
+    }
+
+    sort(p.begin(), p.end(), compare);
+
+    for (auto elem : p) answer.push_back(elem.second);
+
+    return answer;
+}
