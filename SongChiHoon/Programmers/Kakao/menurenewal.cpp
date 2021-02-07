7833da1b3b4eb32e2f8d32ebecc9a1d577fe93a9
marhead <clgns0415@gmail.com>
2021-02-07 21:02:40 +0900

    python

---
+++ SongChiHoon/Programmers/Kakao/menurenewal.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answer;
+    
+    return answer;
+}
