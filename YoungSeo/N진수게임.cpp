b054d1aab987e27a6518224632d814569207d204
chldudtj1111 <71889004+chldudtj1111@users.noreply.github.com>
2021-03-01 20:20:32 +0900

    Update N진수게임.cpp

--- YoungSeo/N진수게임.cpp
+++ YoungSeo/N진수게임.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-string Convert_To_N(int a, int b){
+string Convert_To_N(int a, int n){
     string result = "";
     char list[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
     
