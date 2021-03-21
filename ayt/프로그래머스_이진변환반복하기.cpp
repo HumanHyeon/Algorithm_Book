5759a29302657d864311cade8d86a44626eceaf3
yutae <42912205+y00913@users.noreply.github.com>
2021-03-22 00:09:26 +0900

    Update 프로그래머스_이진변환반복하기.cpp

--- ayt/프로그래머스_이진변환반복하기.cpp
+++ ayt/프로그래머스_이진변환반복하기.cpp
@@ -1,7 +1,5 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
@@ -9,26 +7,27 @@ vector<int> solution(string s) {
     vector<int> answer;
     int cnt = 0;
     int cnt2 = 0;
-    string tmp = "";
     
     while(1){
-      if(tmp.size()==1) break;
+      string tmp = "";
+      
+      if(s=="1") break;
       ++cnt2;
       
       for(int i=0;i<s.size();++i){
         if(s[i]=='1'){
           tmp += "1";
         }
+        else ++cnt;
       }
       int size = tmp.size();
       tmp = "";
       while(size>0){
-        if(size%2==1) tmp+="1";
-        else tmp+="0";
+        if(size%2==1) tmp="1"+tmp;
+        else tmp="0"+tmp;
         size /=  2;
       }
-      reverse(tmp.begin(), tmp.end());
-      cout << tmp << endl;
+      s=tmp;
     }
     
     answer.push_back(cnt2);
