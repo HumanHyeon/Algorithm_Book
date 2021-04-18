066133aa9a4c57e03753bff41eead01cf033421c
msrgo96 <53008468+msrgo96@users.noreply.github.com>
2021-04-18 16:41:02 +0900

    Create 210418dfs(failed).py

---
+++ MoonSeong/210418dfs(failed).py
@@ -0,0 +1,26 @@
+def dfs(x, computers, remainder):
+    contained = []
+    for i in range(len(computers[x])):
+        if computers[x][i] == 1 and computers[x][i] in remainder:
+            contained.append(i)
+    for _ in contained:
+        remainder.remove(_)
+    for c in contained:
+        dfs(c, computers, remainder)
+    
+def solution(n, computers):
+    answer = 0
+
+    remainder = []
+    for i in range(n):
+        remainder.append(i)
+
+    while len(remainder) != 0:
+        dfs(0, computers, remainder)
+        answer += 1
+    
+    print(answer)
+
+    return answer
+
+solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
