f708bac9c309a6a09589708fb94064a24a6e309e
Marhead <clgns0415@gmail.com>
2021-05-30 15:46:29 +0900

    Create 2diffbits.py

---
+++ SongChiHoon/Programmers/2diffbits.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    answer = []
+
+    for number in numbers:
+        bin_number = list('0' + bin(number)[2:])
+        idx = ''.join(bin_number).rfind('0')
+        bin_number[idx] = '1'
+        
+        if number % 2 == 1:
+            bin_number[idx+1] = '0'
+        
+        answer.append(int(''.join(bin_number), 2))
+
+    return answer
