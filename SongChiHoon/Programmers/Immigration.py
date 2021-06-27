fafbe6df869e4e06fb6edfecc720880e9e1ac422
Marhead <clgns0415@gmail.com>
2021-06-27 15:53:26 +0900

    Create Immigration.py

---
+++ SongChiHoon/Programmers/Immigration.py
@@ -0,0 +1,21 @@
+def solution(n, times) :
+  answer = 0
+  left = 1
+  right = max(times) * n
+  
+  while left < right : 
+    mid = (left + right) // 2
+    total = 0
+    
+    for t in times :
+      total += mid // t
+      
+    if total >= n :
+      right = mid
+      
+    else :
+      left = mid + 1
+      
+  answer = left
+  
+  return answer
