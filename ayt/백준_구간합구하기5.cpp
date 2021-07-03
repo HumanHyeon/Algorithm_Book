073e5f396592a65584eac4aa70717f7fdd2b71db
yutae <42912205+y00913@users.noreply.github.com>
2021-07-03 18:26:54 +0900

    Update 백준_구간합구하기5.cpp

--- ayt/백준_구간합구하기5.cpp
+++ ayt/백준_구간합구하기5.cpp
@@ -3,43 +3,43 @@
 
 using namespace std;
 
-int solution(vector<vector<int>> table, vector<int> xy){
-  int sum = 0;
+int solution(vector<vector<int>> table, vector<int> xy) {
+    int sum = 0;
+    int dx = xy[2] - xy[0] + 1;
+    int dy = xy[3] - xy[1] + 1;
 
-  for(int i=xy[0]-1;i<xy[2];i++) {
-    for(int j=xy[1]-1;j<xy[3];j++) {
-      sum += table[i][j];
+    for (int i = xy[1] - 1; i < xy[3]; i++) {
+        sum += table[xy[0] - 1][i];
     }
-  }
 
-  return sum;
+    int tmp = 0;
+    for (int i = 1; i < dx; i++)
+        tmp += i*dy;
+    sum = sum * dx + tmp;
+
+    return sum;
 }
 
 int main() {
-  int N,M;
-  vector<vector<int>> table;
-  vector<vector<int>> xy;
+    int N, M;
+    cin >> N >> M;
 
-  cin >> N >> M;
+    vector<vector<int>> table(N, vector<int>(N));
+    vector<vector<int>> xy(M, vector<int>(4));
 
-  for(int i=0;i<N;i++) {
-    int* a = new int(N);
-    vector<int> tmp;
-    for(int j=0;j<N;j++)
-      cin >> a[j];
-    for(int j=0;j<N;j++) tmp.push_back(a[j]);
-    table.push_back(tmp);
-  }
 
-  for(int i=0;i<M;i++) {
-    int* a = new int(N);
-    vector<int> tmp;
-    for(int j=0;j<N;j++)
-      cin >> a[j];
-    for(int j=0;j<N;j++) xy.push_back(a[j]);
-    xy.push_back(tmp);
-  }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> table[i][j];
+        }
+    }
 
-  for(auto elem : xy)
-    cout << solution(table, elem) << endl;
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> xy[i][j];
+        }
+    }
+
+    for (auto elem : xy)
+        cout << solution(table, elem) << endl;
 }
