a84289635846314b2faa3dd3a84082ffb6547fe0
HumanHyeon <54471641+HumanHyeon@users.noreply.github.com>
2021-07-03 18:10:04 +0900

    Update 21.07.03 Coding Practice No.64.cpp

--- SeHyeon/21.07.03 Coding Practice No.64.cpp
+++ SeHyeon/21.07.03 Coding Practice No.64.cpp
@@ -33,7 +33,7 @@ int main() {
 			dp[i][j] = board[i][j];
 			if ((i - 1) >= 0) dp[i][j] += dp[i - 1][j];
 			if ((j - 1) >= 0) dp[i][j] += dp[i][j - 1];
-			if ((i - 1) >= 0 && (j - 1) >= 0)	dp[i][j] -= dp[i - 1][j - 1];
+			if ((i - 1) >= 0 && (j - 1) >= 0) dp[i][j] -= dp[i - 1][j - 1];
 		}
 	}
 	for (int i = 0; i < m; i++) {
