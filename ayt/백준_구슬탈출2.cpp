ae1fc8bd51c3b5fccedc6c6e07abd7228c678ac8
yutae <ayt0913@gmail.com>
2021-08-01 16:49:34 +0900

    Update 백준_구슬탈출2.cpp

--- ayt/백준_구슬탈출2.cpp
+++ ayt/백준_구슬탈출2.cpp
@@ -1,70 +1,113 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-int oy, ox;
-int by, bx;
-int ry, rx;
-
-int dy[4] = { -1,1,0,0 };
-int dx[4] = { 0,0,-1,1 };
-
-void solution(vector<vector<char>> a, int count, int& m) {
-	if (count >= 10) {
-		m = -1;
-		return;
+class Pt {
+public:
+	int y;
+	int x;
+	Pt() {};
+	Pt(int y, int x) : y(y), x(x) {};
+	void setPt(int y, int x) {
+		this->y = y;
+		this->x = x;
 	}
-	else {
-		for (int i = 0; i < 4; i++) {
-			count++;
+};
 
-			while (a[ry + dy[i]][rx + dx[i]] == '.') {
-				ry += dy[i];
-				rx += dx[i];
-				if (ry == oy && rx == ox) {
-					m = min(count, m);
-					return;
-				}
+const int MAX_TRY = 10;
+const int INF = 987654321;
+//상하좌우
+const int dy[4] = { -1, 1, 0, 0 };
+const int dx[4] = { 0, 0, -1, 1 };
+
+int N, M;	//세로, 가로
+char MAP[10][10];
+Pt R, B, O;
+
+int solve(int cnt, Pt r, Pt b, int dir) {
+	if (cnt > MAX_TRY) {
+		return INF;
+	}
+
+	int ry = r.y;
+	int rx = r.x;
+	int by = b.y;
+	int bx = b.x;
+
+	int res = INF;
+	if (dir != -1) {
+		while (MAP[ry + dy[dir]][rx + dx[dir]] == '.') {
+			ry += dy[dir];
+			rx += dx[dir];
+			if (ry == O.y && rx == O.x) {
+				res = cnt;
+				break;
 			}
+		}
+		while (MAP[by + dy[dir]][bx + dx[dir]] == '.') {
+			by += dy[dir];
+			bx += dx[dir];
+			if (by == O.y && bx == O.x) {
+				return res = INF;
+			}
+		}
+		if (res != INF) return res;
 
-			while (a[by + dy[i]][bx + dx[i]] == '.') {
-				by += dy[i];
-				bx += dx[i];
-				if (by == oy && bx == ox) {
-					m = 987654321;
-					return;
-				}
+		//움직임이 없을때 return;
+		if (ry == r.y && rx == r.x && by == b.y && bx == b.x) return res = INF;
+
+		//겹쳤을 때
+		if (ry == by && rx == bx) {
+			if (dir == 0) {
+				if (r.y < b.y) by++;
+				else ry++;
+			}
+			else if (dir == 1) {
+				if (r.y > b.y) by--;
+				else ry--;
+			}
+			else if (dir == 2) {
+				if (r.x < b.x) bx++;
+				else rx++;
+			}
+			else if (dir == 3) {
+				if (r.x > b.x) bx--;
+				else rx--;
 			}
 		}
 	}
+
+	for (int i = 0; i < 4; i++) {
+		res = min(res, solve(cnt + 1, Pt(ry, rx), Pt(by, bx), i));
+	}
+
+	return res;
 }
 
 int main() {
-	int N, M;
+
 	cin >> N >> M;
-
-	vector<vector<char>> a(N, vector<char>(M));
-
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cin >> a[i][j];
+			cin >> MAP[i][j];
+			if (MAP[i][j] == 'R') {
+				MAP[i][j] = '.';
+				R.setPt(i, j);
+			}
+			else if (MAP[i][j] == 'B') {
+				MAP[i][j] = '.';
+				B.setPt(i, j);
+			}
+			else if (MAP[i][j] == 'O') {
+				MAP[i][j] = '.';
+				O.setPt(i, j);
+			}
 		}
 	}
 
-	for (int i = 0; i < a.size(); i++) {
-		for (int j = 0; j < a[0].size(); j++) {
-			if (a[i][j] == 'O') oy = i, ox = j;
-			if (a[i][j] == 'B') by = i, bx = j;
-			if (a[i][j] == 'R') ry = i, rx = j;
-		}
-	}
-
-	int m = 10;
-	solution(a, 0, m);
-
-	cout << m << endl;
+	int res = solve(0, R, B, -1);
+	cout << (res == INF ? -1 : res) << endl;
 
 	return 0;
 }
