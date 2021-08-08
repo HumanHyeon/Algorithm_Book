dad10cdd33e3643af704f219b858437357d43d9a
yutae <ayt0913@gmail.com>
2021-08-08 15:39:21 +0900

    Update 백준_후위표기식.cpp

--- ayt/백준_후위표기식.cpp
+++ ayt/백준_후위표기식.cpp
@@ -4,51 +4,51 @@
 
 using namespace std;
 
-
 void solve(string N) {
-	vector<char> v1; // 알파벳
-	vector<char> v2; // 괄호
-	vector<char> v3; // 연산
-	bool ck = false;
+	vector<char> v;
+    string result;
 
-	for (auto elem : N) {
-		if (elem >= 'A' && elem <= 'Z') {
-			v1.push_back(elem);
-			if (ck && v2.empty()) {
-				int s = v3.size();
-				for (int i = 0; i < s; i++) {
-					v1.push_back(v3.back());
-					v3.pop_back();
-				}
-				ck = false;
-			}
-		}
-		else {
-			if (elem == '(') {
-				v2.push_back(elem);
-			}
-			else if (elem == ')') {
-				v2.pop_back();
-				v1.push_back(v3.back());
-				v3.pop_back();
-			}
-			else {
-				if (elem == '*' || elem == '/') {
-					v3.push_back(elem);
-					ck = true;
-				}
-				else v3.push_back(elem);
-			}
-		}
-	}
+    for (auto elem : N) {
+        if ('A' <= elem && elem <= 'Z')
+            result += elem;
+        else {
+            switch (elem) {
+            case '(':
+                v.push_back(elem);
+                break;
+            case '*':
+            case '/':
+                while (!v.empty() && (v.back() == '*' || v.back() == '/')) {
+                    result += v.back();
+                    v.pop_back();
+                }
+                v.push_back(elem);
+                break;
+            case '+':
+            case '-':
+                while (!v.empty() && v.back() != '(') {
+                    result += v.back();
+                    v.pop_back();
+                }
+                v.push_back(elem);
+                break;
+            case ')':
+                while (!v.empty() && v.back() != '(') {
+                    result += v.back();
+                    v.pop_back();
+                }
+                v.pop_back();
+                break;
+            }
+        }
+    }
 
-	int s = v3.size();
-	for (int i = 0; i < s; i++) {
-		v1.push_back(v3.back());
-		v3.pop_back();
-	}
+    while (!v.empty()) {
+        result += v.back();
+        v.pop_back();
+    }
 
-	for (auto elem : v1) cout << elem;
+    cout << result;
 }
 
 int main() {
