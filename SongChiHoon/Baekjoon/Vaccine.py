040c1f4bccdcfad31d975518ee9a9b309b734947
Marhead <clgns0415@gmail.com>
2021-08-29 14:59:04 +0900

    Update Vaccine.py

--- SongChiHoon/Baekjoon/Vaccine.py
+++ SongChiHoon/Baekjoon/Vaccine.py
@@ -34,7 +34,7 @@ else:
                 lst.append((i+1,j))
         if j<M-1:
             if before[i][j+1] == before_num:
-                lst.append((i,j+1))    
+                lst.append((i,j+1))
 
         if not lst: # 모든 좌표를 사용한경우 
             break   
