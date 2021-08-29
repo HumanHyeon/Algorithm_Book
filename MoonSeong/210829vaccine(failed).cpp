267cd6bb5337e96191444e0228f44db50e6ed9ec
msrgo96 <53008468+msrgo96@users.noreply.github.com>
2021-08-29 16:30:07 +0900

    Update and rename 210829vaccine(INCOMPLETE).cpp to 210829vaccine(failed).cpp

--- MoonSeong/210829vaccine(failed).cpp
+++ MoonSeong/210829vaccine(failed).cpp
@@ -5,6 +5,8 @@
 #pragma warning (disable:4996)
 using namespace std;
 
+void push(int i, int j, set<pair<int, int>>& pool, vector<vector<int>>& before, vector<vector<int>>& after, int bVal, int aVal);
+
 int main() {
     //input
     int n, m;   //세로 n, 가로 m
@@ -26,12 +28,14 @@ int main() {
     
     //procedure
     set<pair<int, int>> pool;
+    bool diff = false;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (before[i][j] == after[i][j])
                 continue;
             else {
-                auto it = pool.find(pair(i, j));
+                diff = true;
+                auto it = pool.find(pair<int, int>(i, j));
                 if (it == pool.end()) { //not found
                     if (pool.empty()) {     //pool is empty
                         push(i, j, pool, before, after, before[i][j], after[i][j]);
@@ -47,20 +51,38 @@ int main() {
             }
         }
     }
-    
 
+    if (!diff) { 
+        cout << "YES";
+        return 0;
+    }
 
-
+    auto it = pool.begin();
+    int bVal = before[it->first][it->second];
+    int aVal = after[it->first][it->second];
+    for (; it != pool.end(); it++)
+        if (aVal != after[it->first][it->second] || bVal != before[it->first][it->second]) {
+            cout << "NO";
+            return 0;
+        }
+    cout << "YES";
+    return 0;
 }
 
 void push(int i, int j, set<pair<int, int>>& pool, vector<vector<int>>& before, vector<vector<int>>& after, int bVal, int aVal) {
-    set<pair<int, int>>::iterator it[5];
-    it[0] = pool.find(pair(i, j));
-    it[1] = pool.find(pair(i+1, j));
-    it[2] = pool.find(pair(i, j+1));
-    it[3] = pool.find(pair(i-1, j));
-    it[4] = pool.find(pair(i, j-1));
+    pool.insert(pair<int, int>(i, j));
+    set<pair<int, int>>::iterator it[4];
+    pair<int, int> dir4[4] = {pair<int, int>(i+1, j), pair<int, int>(i, j+1), pair<int, int>(i-1, j), pair<int, int>(i, j-1)};
+    for (int k = 0; k < 4; k++)
+        it[k] = pool.find(dir4[k]);
     
-    
+    for (int k = 0; k < 4; k++) {
+        if (it[k] != pool.end())    //found
+            continue;
+        if (dir4[k].first < 0 || before.size() <= dir4[k].first || dir4[k].second < 0 || before[0].size() <= dir4[k].second)  //Out Of Range
+            continue;
+        if (before[dir4[k].first][dir4[k].second] == bVal && after[dir4[k].first][dir4[k].second] == aVal)
+            push(dir4[k].first, dir4[k].second, pool, before, after, bVal, aVal);
+    }
 
 }
