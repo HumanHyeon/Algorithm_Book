142176638afe2492e97fd7093bf01cfcf51f7ec4
yutae <ayt0913@gmail.com>
2021-08-29 14:23:45 +0900

    Create 백준_헤이카카오.cpp

---
+++ ayt/백준_헤이카카오.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+void solution(int a, double d, double k) {
+	float sum = a * d;
+	vector<double> win;
+
+	while (d <= 1) {
+		win.push_back(d);
+		d = d + d * k;
+	}
+	win.push_back(1);
+
+	float tmp = 1;
+	for (int i = 1; i < win.size(); i++) {
+		tmp *= (1 - win[i-1]);
+		sum += a * (i + 1) * tmp * win[i];
+	}
+
+	printf("%.7f", sum);
+}
+
+int main() {
+	int a, d, k;
+	cin >> a >> d >> k;
+
+	solution(a, (double)d/100, (double)k/100);
+
+	return 0;
+}
