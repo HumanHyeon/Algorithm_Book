40070489aa8e26c93ad4a7dca51d3635edd9bab7
yutae <ayt0913@gmail.com>
2021-09-12 13:53:48 +0900

    Update 프로그래머스_모음사전.cpp

--- ayt/프로그래머스_모음사전.cpp
+++ ayt/프로그래머스_모음사전.cpp
@@ -30,3 +30,21 @@ int solution(string word) {
 
     return answer;
 }
+
+
+-------------------------------------------------------------------
+    
+//
+//#include <string>
+//
+//using namespace std;
+//
+//int solution(string word) {
+//    string v = string("AEIOU");
+//    int a = word.size();
+//
+//    for (int i = 0, b = 1; i < word.size(); i++, b *= 5)
+//        a += v.rfind(word[i]) * 781 / b;
+//
+//    return a;
+//}
