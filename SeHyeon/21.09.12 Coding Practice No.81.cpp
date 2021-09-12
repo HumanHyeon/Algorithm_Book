7bbb7f4936edf57552fa9dd0793d7eaea7abe20e
Park SeHyeon <54471641+HumanHyeon@users.noreply.github.com>
2021-09-12 14:12:32 +0900

    Create 21.09.12 Coding Practice No.81.cpp

---
+++ SeHyeon/21.09.12 Coding Practice No.81.cpp
@@ -0,0 +1,19 @@
+https://programmers.co.kr/learn/courses/30/lessons/84512
+
+const char charset[] = { 'A', 'E', 'I', 'O', 'U' };
+const int expr[] = { 781, 156, 31, 6, 1 };
+
+int solution(string word) {
+    int answer = 0;
+
+    for (int i = 0; i < word.size(); i++) {
+        int index;
+        
+        for (index = 0; index < 5; i++)
+            if (charset[index] == word[i])
+                break;
+        answer += (expr[i] * (index) + 1);
+    }
+
+    return answer;
+}
